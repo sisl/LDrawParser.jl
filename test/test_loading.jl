9a0b15c86fd5ca88bf7e03cbbce3e9e638b38b0e
Kyle Brown <kylejbrown17@gmail.com>
2021-01-20 00:54:54 -0800

    The last commit worked! At least, it allowed the Github runner to successfully test the package (the tests failed because they need access to the ldraw parts library, which is external to the repository). So it appears that the key was to explicitly add the general registry in addition to mine. Trying now with the standard build-pkg action, which will add GraphUtils as an add dependency rather than a dev dependency.
    Also added a check to see if the LDRAW_PARTS_LIBRARY exists before
    running populate_part_geometry!(...) tests.

--- test/test_loading.jl
+++ test/test_loading.jl
@@ -8,9 +8,13 @@ let
     @test isempty(model.sub_parts)
     @test !isempty(model.models["20009 - AT-TE Walker.mpd"].steps[1].lines)
     # load geometry
-    LDrawParser.populate_part_geometry!(model)
-    for (k,p) in Base.Iterators.flatten((model.parts,model.sub_parts))
-        @assert p.populated.status == true
+    if isdir(get_part_library_dir())
+        LDrawParser.populate_part_geometry!(model)
+        for (k,p) in Base.Iterators.flatten((model.parts,model.sub_parts))
+            @assert p.populated.status == true
+        end
+    else
+        @warn "Skipping tests for populate_part_geometry!(...) because $(get_part_library_dir()) does not exist"
     end
 
 end
