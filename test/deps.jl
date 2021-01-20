46d0d5bbc967573f184c9fc81ec67c1e52166e39
Kyle Brown <kylejbrown17@gmail.com>
2021-01-19 22:54:30 -0800

    Trying a dev of GraphUtils.

--- test/deps.jl
+++ test/deps.jl
@@ -1,4 +1,4 @@
 # A script for loading unregistered dependencies before CI action
 using Pkg
 
-pkg"add https://github.com/kylejbrown17/GraphUtils.jl.git"
+pkg"dev https://github.com/kylejbrown17/GraphUtils.jl.git"
