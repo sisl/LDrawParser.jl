46ce403291a2cc9b380d77dcf1902c67e8b3278d
Dylan Asmar <asmar@stanford.edu>
2023-10-18 14:40:25 -0700

    project clean-up

--- test/test_parsing.jl
+++
@@ -1,5 +0,0 @@
-let
-    @test LDrawParser.parse_meta_command(["0", "ROTSTEP", "END"]) == LDrawParser.ROTSTEP_END
-    @test LDrawParser.parse_meta_command(["0", "ROTSTEP", ""]) == LDrawParser.ROTSTEP_BEGIN
-    @test LDrawParser.parse_meta_command(["0", "STEP", ""]) == LDrawParser.STEP
-end
