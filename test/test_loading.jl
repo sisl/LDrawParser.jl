034cbca47ea3dd65ce435e7959e15ee919219a22
Dylan Asmar <asmar@stanford.edu>
2023-10-19 00:44:19 -0700

    more parts so tests work

--- test/test_loading.jl
+++ test/test_loading.jl
@@ -1,3 +1,9 @@
+
+orig_part_dir = LDrawParser.get_part_library_dir()
+
+# set the temporary parts library for testing
+LDrawParser.set_part_library_dir!(joinpath(dirname(pathof(LDrawParser)), "..", "assets", "test_parts_lib"))
+
 let
     filename = joinpath(dirname(pathof(LDrawParser)), "..", "assets", "20009-1 - AT-TE Walker - Mini.mpd")
     model = parse_ldraw_file(filename; ignore_rotation_determinant=true)
@@ -10,13 +16,7 @@ let
     @test length(model.models["20009 - Light Leg.ldr"].steps) == 4
     @test length(model.models["20009 - Turret.ldr"].steps) == 7
 
-    # load geometry, need ldraw parts library for this to work
-    if !isdir(get_part_library_dir())
-        # set the temporary one for testing
-        LDrawParser.set_part_library_dir!(joinpath(dirname(pathof(LDrawParser)), "..", "assets", "test_parts_lib"))
-    end
 
-    LDrawParser.set_part_library_dir!(joinpath(dirname(pathof(LDrawParser)), "..", "assets", "test_parts_lib"))
     LDrawParser.populate_part_geometry!(model; ignore_rotation_determinant=true)
     @test !isempty(model.sub_parts)
     for (k, p) in Base.Iterators.flatten((model.parts, model.sub_parts))
@@ -36,23 +36,21 @@ let
     @test length(model.models["21309 - 136-243.ldr"].steps) == 14
 
     # load geometry, need ldraw parts library for this to work
-    if isdir(get_part_library_dir())
-        LDrawParser.populate_part_geometry!(model; ignore_rotation_determinant=true)
-        @test !isempty(model.sub_parts)
-        for (k, p) in Base.Iterators.flatten((model.parts, model.sub_parts))
-            @assert p.populated.status == true
-        end
-    else
-        @warn "Skipping tests for populate_part_geometry!(...) because $(get_part_library_dir()) does not exist"
+    LDrawParser.populate_part_geometry!(model; ignore_rotation_determinant=true)
+    @test !isempty(model.sub_parts)
+    for (k, p) in Base.Iterators.flatten((model.parts, model.sub_parts))
+        @assert p.populated.status == true
     end
 end
 let
     filename = joinpath(dirname(pathof(LDrawParser)), "..", "assets", "6339-1 - Shuttle Launch Pad.mpd")
     model = parse_ldraw_file(filename; ignore_rotation_determinant=true)
-    if isdir(get_part_library_dir())
-        LDrawParser.populate_part_geometry!(model; ignore_rotation_determinant=false)
-        ldraw_color_dict = LDrawParser.get_color_dict()
-    end
+    LDrawParser.populate_part_geometry!(model; ignore_rotation_determinant=false)
+    ldraw_color_dict = LDrawParser.get_color_dict()
     LDrawParser.change_coordinate_system!(model, ldraw_base_transform(), 0.1; ignore_rotation_determinant=true)
     get_part(model, 0)
+    get_part(model, "3938.dat")
 end
+
+# reset the parts library
+LDrawParser.set_part_library_dir!(orig_part_dir)
